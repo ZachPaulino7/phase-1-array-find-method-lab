855bae9e3daadd2ad27bb31916165e9f127e3bf3
Zachpaulino7 <Zachp7@live.com>
2021-08-20 02:42:24 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+//let superbowlWin = record.find(record => record.result === 'W')
 
+function superbowlWin(record) {
+    let sbWin = record.find(record => record.result === "W") 
+        if(sbWin) { 
+            return sbWin.year
+        }
+}
